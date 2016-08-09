229292c9823902255a642114296383316cf86209
Marshina <tcetcya@mail.ru>
2016-08-09 15:00:15 +0300

    Delete Task3_Function.js

--- Task3_Function.js
+++
@@ -1,9 +0,0 @@
-function isEven(n) {
-  number = Math.abs(number);
-  if (number === 0) {
-    return true;
-  } else if (number === 1) {
-    return false;
-  } else
-    return isEven(number - 2);
-}
